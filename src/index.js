00333949770641128633cfe6c2a717b01f4d9b1d
Aakash Rajput <aakash.rajput-external@tcs.com>
2024-06-22 18:13:08 +0530

    DB connected and utils setup

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// import dotenv from "dotenv";
+import "dotenv/config";
+
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+// dotenv.config({
+//   path: "./env",
+// });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env || 800, () => {
+      console.log(`Server is running at port ${process.env.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed", err);
+  });
+
+/*
+(async()=>{
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log("ERROR",error)
+        throw error
+    })
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT} `)
+    })
+  } catch (error) {
+    console.error("ERROR",error)
+    throw error
+  }
+})()
+  */
